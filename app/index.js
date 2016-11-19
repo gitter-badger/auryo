5a3415fb68d18014f492cdcbe8be0f96ceab72b6
Superjo149 <jo.snel@hotmail.com>
2016-11-19 16:00:56 +0100

    Header next & prev buttons

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
-import {Router, hashHistory, Route, IndexRoute} from "react-router";
+import {Router, hashHistory, Route, IndexRoute,createMemoryHistory} from "react-router";
 import {syncHistoryWithStore} from "react-router-redux";
 import configureStore from "./_common/store/configureStore";
 import "./assets/css/app.scss";
@@ -10,10 +10,14 @@ import App from "./App";
 import Feed from "./_Feed/feedContainer";
 import SongDetails from "./_SongDetails/songContainer";
 
+import HistoryTracker from 'back-forward-history'
+
 
 const store = configureStore();
 
-const history = syncHistoryWithStore(hashHistory, store);
+const history = syncHistoryWithStore(createMemoryHistory(), store);
+
+HistoryTracker.listenTo(history)
 
 render(
   <Provider store={store}>
