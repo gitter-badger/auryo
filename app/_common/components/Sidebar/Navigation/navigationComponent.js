e2043e43e080a7957a5309ff0e55f759cafa2d35
Superjo149 <jo.snel@hotmail.com>
2016-11-19 17:31:18 +0100

    hidden incomplete pages + renamed Feed to stream

--- app/_common/components/Sidebar/Navigation/navigationComponent.js
+++ app/_common/components/Sidebar/Navigation/navigationComponent.js
@@ -12,29 +12,33 @@ class Navigation extends Component {
         <ul className="nav">
           <li className="navItem">
             <IndexLink to="/" className="navLink" activeClassName="active">
-              <i className="icon-home" />
-              <span>Feed</span>
+              <i className="icon-audiotrack" />
+              <span>Stream</span>
             </IndexLink>
           </li>
+          {/*
           <li className="navItem">
-            <Link to="/chart" className="navLink" activeClassName="active">
-              <i className="icon-poll" />
-              <span>Charts</span></Link>
-          </li>
+           <Link to="/chart" className="navLink" activeClassName="active">
+           <i className="icon-poll" />
+           <span>Charts</span></Link>
+           </li>
+           */}
         </ul>
-        <h2>Me</h2>
-        <ul className="nav">
-          <li className="navItem">
-            <Link to="/likes" className="navLink" activeClassName="active">
-              <i className="icon-favorite" />
-              <span>Likes</span></Link>
-          </li>
-          <li className="navItem">
-            <Link to="/playlists" className="navLink" activeClassName="active">
-              <i className="icon-album" />
-              <span>Playlists</span></Link>
-          </li>
-        </ul>
+        {/*
+         <h2>Me</h2>
+         <ul className="nav">
+         <li className="navItem">
+         <Link to="/likes" className="navLink" activeClassName="active">
+         <i className="icon-favorite" />
+         <span>Likes</span></Link>
+         </li>
+         <li className="navItem">
+         <Link to="/playlists" className="navLink" activeClassName="active">
+         <i className="icon-album" />
+         <span>Playlists</span></Link>
+         </li>
+         </ul>
+         */}
       </div>
     );
   }
