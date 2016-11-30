111a57004198aa801f7ac112509942192b66b0d6
jonassnellinckx <jo.snel@hotmail.com>
2016-11-30 22:19:42 +0100

    Fix audio stopping after internet disconnect

--- app/_common/components/Audio.js
+++ app/_common/components/Audio.js
@@ -2,147 +2,158 @@ import React, {PropTypes, Component} from "react";
 import ReactDOM from "react-dom";
 
 const status = {
-  PLAYING: 'PLAYING',
-  STOPPED: 'STOPPED',
-  PAUSED: 'PAUSED'
+    PLAYING: 'PLAYING',
+    STOPPED: 'STOPPED',
+    PAUSED: 'PAUSED'
 };
 
 class Audio extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      status: status.STOPPED,
-    };
+        this.state = {
+            status: status.STOPPED,
+        };
 
-    this.onError = this.onError.bind(this);
+        this.onError = this.onError.bind(this);
 
-    // html audio element used for playback
-    this.audio = null;
-  }
-
-  componentDidMount() {
-
-    const audio = this.audio = ReactDOM.findDOMNode(this.refs.audio);
-    audio.preload = 'metadata';
-    audio.addEventListener('ended', this.props.onFinishedPlaying);
-    audio.addEventListener('timeupdate', () => {
-      if (this.audio) {
-        this.props.onPlaying(this.audio.currentTime);
-      }
-    });
-
-    audio.addEventListener('loadedmetadata', () => {
-      if (this.audio) {
-        this.props.onLoading(this.audio.duration);
-      }
-    });
-
-    audio.addEventListener('play', () => this.toggleStatus(status.PLAYING));
-
-    audio.addEventListener('stalled', () => this.toggleStatus(status.PAUSED));
-
-    this.toggleStatus(this.props.playStatus);
-  }
-
-  componentWillUnmount() {
-
-    /* pause the audio element before dereferencing it
-     * (we can't know when garbage collection will run)
-     */
-    this.audio.pause();
-    this.audio = null;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.playStatus != nextProps.playStatus) {
-      this.toggleStatus(nextProps.playStatus);
+        // html audio element used for playback
+        this.audio = null;
     }
 
-    if (this.props.playFromPosition !== nextProps.playFromPosition) {
-      this.audio.currentTime = nextProps.playFromPosition;
+    componentDidMount() {
+
+        const audio = this.audio = ReactDOM.findDOMNode(this.refs.audio);
+        audio.preload = 'auto';
+        audio.addEventListener('ended', this.props.onFinishedPlaying);
+        audio.addEventListener('timeupdate', () => {
+            if (this.audio) {
+                this.props.onPlaying(this.audio.currentTime);
+            }
+        });
+
+        audio.addEventListener('loadedmetadata', () => {
+            if (this.audio) {
+                this.props.onLoading(this.audio.duration);
+            }
+        });
+
+        audio.addEventListener('play', () => this.toggleStatus(status.PLAYING));
+
+        audio.addEventListener('stalled', () => this.toggleStatus(status.PAUSED));
+
+        this.toggleStatus(this.props.playStatus);
     }
 
-    if (this.props.url != nextProps.url || this.props.id != nextProps.id) {
+    componentWillUnmount() {
 
-      if (this.state.status == status.PLAYING) {
+        /* pause the audio element before dereferencing it
+         * (we can't know when garbage collection will run)
+         */
         this.audio.pause();
-      }
-      this.audio.src = nextProps.url;
-      this.audio.play();
+        this.audio = null;
     }
 
-    if (this.props.volume != nextProps.volume) {
-      this.audio.volume = nextProps.volume / 100;
+    componentWillReceiveProps(nextProps) {
+        if (this.props.playStatus != nextProps.playStatus) {
+            this.toggleStatus(nextProps.playStatus);
+        }
+
+        if (this.props.playFromPosition !== nextProps.playFromPosition) {
+            this.audio.currentTime = nextProps.playFromPosition;
+        }
+
+        if (this.props.url != nextProps.url || this.props.id != nextProps.id) {
+
+            this.audio.pause();
+            this.audio.currentTime = 0;
+            this.audio.src = nextProps.url;
+            this.audio.load();
+            this.audio.play();
+        }
+
+        if (this.props.volume != nextProps.volume) {
+            this.audio.volume = nextProps.volume / 100;
+        }
+
+        if(this.props.offline != nextProps.offline){
+            const time = this.audio.currentTime;
+            this.audio.load();
+            this.audio.currentTime = time;
+            this.audio.play();
+        }
+
+
     }
 
+    componentDidUpdate() {
 
-  }
-
-  componentDidUpdate() {
-
-  }
-
-  toggleStatus(value) {
-    if (!this.audio) {
-      return;
     }
 
-    if (this.state.status == value) {
-      return;
+    toggleStatus(value) {
+        if (!this.audio) {
+            return;
+        }
+
+        if (this.state.status == value) {
+            return;
+        }
+
+        try {
+            if (value == status.PLAYING) {
+                if(this.state.status !== status.PLAYING){
+                    this.audio.play();
+                }
+            } else if (value == status.PAUSED) {
+                this.audio.pause();
+            } else if (value == status.STOPPED) {
+                this.audio.pause();
+                this.audio.currentTime = 0;
+            }
+        } catch (error) {
+            console.log("error in audio", error)
+        }
+
+        this.setState({
+            status: value
+        });
     }
 
-    try {
-      if (value == status.PLAYING && (this.state.status !== status.PLAYING)) {
-        this.audio.play();
-      } else if (value == status.PAUSED) {
-        this.audio.pause();
-      } else if (value == status.STOPPED) {
-        this.audio.pause();
-        this.audio.currentTime = 0;
-      }
-    } catch (error) {
-      console.log("error in audio", error)
+    onError(e) {
+        const {onError} = this.props;
+
+        onError(e, this.audio);
     }
 
-    this.setState({
-      status: value
-    });
-  }
+    render() {
+        const {url, muted} = this.props;
 
-  onError(e) {
-    const {onError} = this.props;
-
-    onError(e, this.audio);
-  }
-
-  render() {
-    const {url, muted} = this.props;
-
-    return (
-      <audio ref="audio" muted={muted}
-             onError={this.onError}>
-        <source src={url}/>
-      </audio>
-    );
-  }
+        return (
+            <audio ref="audio"
+                   muted={muted}
+                   onError={this.onError}>
+                <source src={url}/>
+            </audio>
+        );
+    }
 
 }
 
 Audio.propTypes = {
-  autoplay: PropTypes.bool,
-  url: PropTypes.string.isRequired,
-  playStatus: PropTypes.string,
-  volume: PropTypes.number.isRequired,
-  playFromPosition: PropTypes.number.isRequired,
-  muted: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired,
+    autoplay: PropTypes.bool,
+    url: PropTypes.string.isRequired,
+    playStatus: PropTypes.string,
+    volume: PropTypes.number.isRequired,
+    playFromPosition: PropTypes.number.isRequired,
+    muted: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
 
-  onLoading: PropTypes.func.isRequired,
-  onPlaying: PropTypes.func.isRequired,
-  onFinishedPlaying: PropTypes.func.isRequired,
-  onError: PropTypes.func.isRequired
+    onLoading: PropTypes.func.isRequired,
+    onPlaying: PropTypes.func.isRequired,
+    onFinishedPlaying: PropTypes.func.isRequired,
+    onError: PropTypes.func.isRequired,
+    offline: PropTypes.bool.isRequired
 };
 
 export default Audio;
