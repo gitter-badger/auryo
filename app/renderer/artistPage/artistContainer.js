85c9fb37d05b6454cb72e826dee1134bc2585ea7
jonassnellinckx <jo.snel@hotmail.com>
2016-12-15 15:16:40 +0100

    Artist page html cleanup

--- app/renderer/artistPage/artistContainer.js
+++ app/renderer/artistPage/artistContainer.js
@@ -52,7 +52,7 @@ class artistContainer extends Component {
     }
 
     toggleFollow() {
-        const {dispatch, auth,params} = this.props;
+        const {dispatch, auth, params} = this.props;
         const {artistId} = params;
 
         dispatch(toggleFollowing(artistId));
@@ -81,73 +81,71 @@ class artistContainer extends Component {
                 playing={false}
                 scrollFunc={fetchMore.bind(this, playlist_name, OBJECT_TYPES.PLAYLISTS)}
                 dispatch={dispatch}
-                className="artistPage">
-                <Container fluid={true} className="artist trackDetails">
-                    <Row className="trackHeader">
+                className="artistPage container-fluid">
+                <Row className="trackHeader row">
 
-                        <div className="overlayWrapper">
+                    <div className="overlayWrapper">
+                        <FallbackImage
+                            offline={app.offline}
+                            dispatch={dispatch}
+                            track_id={user.id}
+                            className="overlayImg"
+                            src={user_img}/>
+                    </div>
+
+                    <Col xs="12" md="4" xl="2">
+                        <div className="imageWrapper">
                             <FallbackImage
                                 offline={app.offline}
                                 dispatch={dispatch}
                                 track_id={user.id}
-                                className="overlayImg"
+                                src={user_img}/>
+
+                            <FallbackImage
+                                offline={app.offline}
+                                dispatch={dispatch}
+                                track_id={user.id}
+                                className="imgShadow"
                                 src={user_img}/>
                         </div>
+                    </Col>
 
-                        <Col xs="12" md="4" xl="2">
-                            <div className="imageWrapper">
-                                <FallbackImage
-                                    offline={app.offline}
-                                    dispatch={dispatch}
-                                    track_id={user.id}
-                                    src={user_img}/>
+                    <Col xs="12" md="8" xl="10" className="trackInfo text-md-left text-xs-center">
 
-                                <FallbackImage
-                                    offline={app.offline}
-                                    dispatch={dispatch}
-                                    track_id={user.id}
-                                    className="imgShadow"
-                                    src={user_img}/>
-                            </div>
-                        </Col>
+                        <Row className="flex-items-md-between">
+                            <Col xs="12" md="6">
+                                <h1 className="trackTitle">{user.username}</h1>
+                                <h2 className="trackArtist">{user.city}{user.city && user.country ? " , " : null }{user.country}</h2>
+                            </Col>
 
-                        <Col xs="12" md="8" xl="10" className="trackInfo text-md-left text-xs-center">
+                            <Col xs="12" md="" className="col-md text-xs-right">
+                                <ul className="artistStats flex flex-items-xs-center flex-items-md-right">
+                                    <li>
+                                        <span>{abbreviate_number(user.followers_count)}</span>
+                                        <span>Followers</span>
+                                    </li>
+                                    <li>
+                                        <span>{abbreviate_number(user.followings_count)}</span>
+                                        <span>Following</span>
+                                    </li>
+                                    <li>
+                                        <span>{abbreviate_number(user.track_count)}</span>
+                                        <span>Tracks</span>
+                                    </li>
+                                </ul>
+                                <a href="javascript:void(0)"
+                                   className={cn("c_btn", {following: following})}
+                                   onClick={this.toggleFollow.bind(this)}>
+                                    {following ? <i className="icon-check"></i> : <i className="icon-add"></i>}
+                                    <span>{following ? "Following" : "Follow"}</span>
+                                </a>
+                            </Col>
+                        </Row>
 
-                            <Row className="flex-items-md-between">
-                                <Col xs="12" md="6">
-                                    <h1 className="trackTitle">{user.username}</h1>
-                                    <h2 className="trackArtist">{user.city}{user.city && user.country ? " , " : null }{user.country}</h2>
-                                </Col>
+                    </Col>
 
-                                <Col xs="12" md="" className="col-md text-xs-right">
-                                    <ul className="artistStats flex flex-items-xs-center flex-items-md-right">
-                                        <li>
-                                            <span>{abbreviate_number(user.followers_count)}</span>
-                                            <span>Followers</span>
-                                        </li>
-                                        <li>
-                                            <span>{abbreviate_number(user.followings_count)}</span>
-                                            <span>Following</span>
-                                        </li>
-                                        <li>
-                                            <span>{abbreviate_number(user.track_count)}</span>
-                                            <span>Tracks</span>
-                                        </li>
-                                    </ul>
-                                    <a href="javascript:void(0)"
-                                       className={cn("c_btn", {following: following})}
-                                       onClick={this.toggleFollow.bind(this)}>
-                                        {following ? <i className="icon-check"></i> : <i className="icon-add"></i>}
-                                        <span>{following ? "Following" : "Follow"}</span>
-                                    </a>
-                                </Col>
-                            </Row>
-
-                        </Col>
-
-                    </Row>
-                </Container>
-                <div className="flex tracktabs">
+                </Row>
+                <div className="flex tracktabs row">
                     <a href="javascript:void(0)" className={cn({active: this.state.activeTab === '1'})}
                        onClick={() => {
                            this.toggle('1');
@@ -155,38 +153,36 @@ class artistContainer extends Component {
                         <span className="text">Tracks</span>
                     </a>
                 </div>
-                <Container fluid>
-                    <Row>
-                        <Col xs="9">
+                <Row>
+                    <Col xs="9">
 
-                            <TabContent activeTab={this.state.activeTab}>
-                                <TabPane tabId="1">
-                                    <TrackList
-                                        dispatch={dispatch}
-                                        likes={likes}
-                                        player={player}
-                                        user_entities={user_entities}
-                                        track_entities={track_entities}
-                                        playlist={playlist_name}
-                                        playlists={playlists}
-                                        likeFunc={this.toggleLike.bind(this)}
+                        <TabContent activeTab={this.state.activeTab}>
+                            <TabPane tabId="1">
+                                <TrackList
+                                    dispatch={dispatch}
+                                    likes={likes}
+                                    player={player}
+                                    user_entities={user_entities}
+                                    track_entities={track_entities}
+                                    playlist={playlist_name}
+                                    playlists={playlists}
+                                    likeFunc={this.toggleLike.bind(this)}
 
-                                    />
+                                />
 
-                                    {playlist.isFetching ? <Spinner/> : null}
+                                {playlist.isFetching ? <Spinner/> : null}
 
-                                </TabPane>
-                            </TabContent>
+                            </TabPane>
+                        </TabContent>
 
-                        </Col>
-                        <Col xs="3" className="artistSide">
-                            <ToggleMoreComponent>
-                                <div dangerouslySetInnerHTML={formatDescription(user.description)}></div>
-                            </ToggleMoreComponent>
+                    </Col>
+                    <Col xs="3" className="artistSide">
+                        <ToggleMoreComponent>
+                            <div dangerouslySetInnerHTML={formatDescription(user.description)}></div>
+                        </ToggleMoreComponent>
 
-                        </Col>
-                    </Row>
-                </Container>
+                    </Col>
+                </Row>
             </InfinityScroll>
         )
     }
